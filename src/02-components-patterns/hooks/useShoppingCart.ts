f84fb1fcb01b899f684b6cfa733fbb1c7dfccaf9
fzarate <fede.zarate.uni@gmail.com>
2022-01-14 15:15:10 -0600

    Control props alternative

--- src/02-components-patterns/hooks/useShoppingCart.ts
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -8,35 +8,21 @@ const useShoppingCart = () => {
     const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
 
         setShoppingCart(oldShoppingcart => {
+            if (count === 0) {
+            //   const data = Object.assign(oldShoppingcart);
+            //   delete data[product.id];
+            //   return {
+            //     ...data,
+            //   };
 
-            const productInCart: ProductInCart = oldShoppingcart[product.id] || { ...product, count: 0 }
-
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count
-                return {
-                    ...oldShoppingcart,
-                    [product.id]: productInCart
-                }
+                const { [product.id]: toDelete, ...rest} = oldShoppingcart;
+                return rest;
             }
 
-            const { [product.id]: toDelete, ...rest } = oldShoppingcart;
-            return rest;
-
-            // if (count === 0) {
-            // //   const data = Object.assign(oldShoppingcart);
-            // //   delete data[product.id];
-            // //   return {
-            // //     ...data,
-            // //   };
-
-            //     const { [product.id]: toDelete, ...rest} = oldShoppingcart;
-            //     return rest;
-            // }
-
-            // return {
-            //     ...oldShoppingcart,
-            //     [product.id]: {...product, count}
-            // }
+            return {
+                ...oldShoppingcart,
+                [product.id]: {...product, count}
+            }
         })
     }
 
